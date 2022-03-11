e36082f2e3e349d287848023aac79a447043b912
Fartash <aq.fartash421@gmail.com>
2022-03-11 10:39:44 +0330

    added function winner() for check who is Winner

--- ProjectFinil.cpp
+++ ProjectFinil.cpp
@@ -69,6 +69,22 @@ void swap_player_and_marker()
 	} 
 	else current_player= 1;
 }
+
+
+char winner(){
+	for(int i=0; i<3;i++){
+		// rows
+		if(board[i][0] == board[i][1] && board[i][1] == board[i][2]) return board[i][0];
+		//col
+			if(board[0][i] == board[1][i] && board[1][i] == board[2][i]) return board[0][i];
+		
+		}
+		//current_player
+	if(board[0][0]== board[1][1] && board[1][1]== board[2][2]) return board[0][0];
+	if(board[0][2]== board[1][1] && board[1][1]== board[2][0]) return board[0][2];
+	
+	return 0;
+}
 	
 
 int main()
